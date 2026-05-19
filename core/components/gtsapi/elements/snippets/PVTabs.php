ed820b802f543ff9c2ea6695ee01d2a7730f2c81
tuniekov <tuniekov@gmail.com>
2026-05-19 13:36:53 +0700

    PVTabs: cache-bust by package update timestamp + http/https via SERVER_PORT

--- core/components/gtsapi/elements/snippets/PVTabs.php
+++ core/components/gtsapi/elements/snippets/PVTabs.php
@@ -1,15 +1,23 @@
 <?php
 /** @var modX $modx */
 /** @var array $scriptProperties */
+$vapi = 6;
+if($package = $modx->getObject('transport.modTransportPackage', ['package_name:LIKE' => '%gtsapi%'])) {
+    $vapi = strtotime($package->updated);
+}
+if($_SERVER['SERVER_PORT'] == 80){
+        $http1 = "http";
+    }else if($_SERVER['SERVER_PORT'] == 443){
+        $http1 = "https";
+    }
+$modx->regClientCSS($modx->getOption('assets_url').'components/gtsapi/js/web/pvtables/pvtables.css?v='.$vapi);
 
-$modx->regClientCSS($modx->getOption('assets_url').'components/gtsapi/js/web/pvtables/pvtables.css');
-
-    $assets_gtsapi_url = $modx->getOption('server_protocol').'://'.$modx->getOption('http_host').$modx->getOption('assets_url').'components/gtsapi/';
+    $assets_gtsapi_url = $http1.'://'.$modx->getOption('http_host').$modx->getOption('assets_url').'components/gtsapi/';
     $imports = [];
     if($load_vue = $modx->getOption('gtsapi_load_vue',null,true)){
         $imports['imports']['vue'] = $assets_gtsapi_url.'js/web/vue.esm-browser.js';
         $pvtables_path = $modx->getOption('assets_path').'components/gtsapi/js/web/pvtables/';
-        $imports['imports']['pvtables/dist/pvtables'] = $assets_gtsapi_url.'js/web/pvtables/pvtables.js';
+        $imports['imports']['pvtables/dist/pvtables'] = $assets_gtsapi_url.'js/web/pvtables/pvtables.js?v='.$vapi;
     }
     
     if(!empty($imports)){
